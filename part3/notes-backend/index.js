077a9d379a6e861648b331c2f9efedca11f41473
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-10-23 13:49:46 -0700

    adding crud routes

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const notes = [
+let notes = [
   {
     id: "1",
     content: "HTML is easy",
@@ -23,8 +23,21 @@ app.get("/", (req, res) => {
     "<h1>Bruh how did browser know to render HTML? </h1><a href=\'/api/notes\' >/notes</a>",
   );
 });
-app.get("/apii/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+app.delete("/api.notes/:id", (req, res) => {
+  const id = request.params.id;
+  notes = notes.filter((n) => n.id !== id);
 });
 const PORT = 4001;
 app.listen(PORT, () => {
