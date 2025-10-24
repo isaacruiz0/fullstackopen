fc6bdb2bb11ec9e0b7e19853632b11bddd89dee3
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2025-10-24 14:56:44 -0700

    added post

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -18,6 +18,10 @@ let notes = [
     important: true,
   },
 ];
+const generateId = () => {
+  notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 app.get("/", (req, res) => {
   res.send(
     "<h1>Bruh how did browser know to render HTML? </h1><a href=\'/api/notes\' >/notes</a>",
@@ -35,9 +39,30 @@ app.get("/api/notes/:id", (req, res) => {
     res.status(404).end();
   }
 });
-app.delete("/api.notes/:id", (req, res) => {
-  const id = request.params.id;
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
   notes = notes.filter((n) => n.id !== id);
+  if (notes) {
+    res.json(notes);
+    res.status(200).end();
+  } else {
+    res.status(404).end();
+  }
+});
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+  notes = note.concat(note);
+  res.json(note);
 });
 const PORT = 4001;
 app.listen(PORT, () => {
